03c8b1e18a254ea512f1daae3ab27151fc45923a
matteo <matteobarbolovici@yahoo.com>
2019-05-23 20:49:31 +0300

    Matteo

---
+++ src/org/fasttrackit/Main.java
@@ -0,0 +1,25 @@
+package org.fasttrackit;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println(2);
+        System.out.println("Hello Matteo");
+
+        char myChar = 'a';
+        byte myByte = 123;
+        short myShort = 12312;
+        int myInt = 1;
+        long myLong = 123121231;
+        float myFloat = 12312;
+         String myString = "MyName";
+
+         int result = myByte +2;
+        System.out.println("Result is: " + (2 + 2));
+        System.out.println( 2 * 5);
+    }
+}
