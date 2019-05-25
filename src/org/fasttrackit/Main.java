d1f3d1b6824d179266b1355905b708215df9b948
matteo <matteobarbolovici@yahoo.com>
2019-05-25 18:46:48 +0300

    homework

--- src/org/fasttrackit/Main.java
+++ src/org/fasttrackit/Main.java
@@ -1,11 +1,9 @@
 package org.fasttrackit;
 
-import java.sql.SQLOutput;
-
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
         System.out.println(2);
         System.out.println("Hello Matteo");
@@ -16,10 +14,17 @@ public class Main {
         int myInt = 1;
         long myLong = 123121231;
         float myFloat = 12312;
-         String myString = "MyName";
+        String myString = "MyName";
 
-         int result = myByte +2;
-        System.out.println("Result is: " + (2 + 2));
-        System.out.println( 2 * 5);
+        int result = myByte + 2;
+        int suma = 2 + 2;
+        System.out.println("suma is: " + (67246 + 27352));
+        System.out.println(2 * 5);
+        double rest = 10 % 3;
+        System.out.println("the remainder is " + rest);
+        System.out.println("Result for 4.A is " + (-5 + 8 * 6));
+        System.out.println("Result for 4.B is " + (55 + 9) % 9);
+        System.out.println("Result for 4.C is " + (20 + -3 * 5 / 8));
+        System.out.println("Result for 4.D is " + (5 + 15 / 3 * 2 - 8 % 3));
     }
 }
