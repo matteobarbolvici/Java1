896bf974d5ce479d7edf315fb6bb8701aafe7d1c
matteo <matteobarbolovici@yahoo.com>
2019-06-04 20:16:57 +0300

    Matteo animal rescue

---
+++ src/lab2/AnimalRescue/AnimalRescue.java
@@ -0,0 +1,12 @@
+package lab2.AnimalRescue;
+
+public class AnimalRescue{
+
+    public static void main(String[] args) {
+        Dog Johnny = new Dog();
+
+        System.out.println(Johnny.breed );
+        Johnny.drink();
+
+    }
+}
