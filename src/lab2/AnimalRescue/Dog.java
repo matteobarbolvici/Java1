3e2bab8f74a934ce29baa1d8cab66ebea78196b6
matteo <matteobarbolovici@yahoo.com>
2019-06-04 20:19:22 +0300

    Matteo DOG

---
+++ src/lab2/AnimalRescue/Dog.java
@@ -0,0 +1,34 @@
+package lab2.AnimalRescue;
+
+public class Dog {
+    String breed = "Husky";
+    int age = 5;
+    float heightM = 1f;
+    float weight = 5;
+    String name = "Johnny";
+
+    public void eat() {
+        System.out.println("He's eating");
+    }
+    public void drink() {
+        System.out.println("Drinking rain water");
+    }
+    public void play() {
+        System.out.println("Play fetch");
+    }
+    public void clothing() {
+        System.out.println("Dog Clothes");
+    }
+    public void medicine() {
+        System.out.println("Dog Medicine");
+    }
+    public void doctor(){
+        System.out.println("Doctor");
+    }
+
+        }
+
+
+
+
+
