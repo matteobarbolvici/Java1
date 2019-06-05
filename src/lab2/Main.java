25ade2e5277b91a983daca6a27a96f3d89b6d945
matteo <matteobarbolovici@yahoo.com>
2019-06-05 19:32:54 +0300

    Matteo Homework

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -4,49 +4,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Sum result is: " + adunare(2,3));
-        System.out.println("Rezultatul scaderii este: " + scadere(452,47));
-        System.out.println("Rezultatul inmultirii este: " + inmultire(5,5));
-        System.out.println("Rezultatul impartirii este: " + impartire(10,2));
-        System.out.println("media numerelor este: " + mediaNumerelor(20,39, 75));
-        System.out.println("temperatura este: " + temperatura(92));
+        System.out.println("Sum result is: " + addition(2,3));
+        System.out.println("Subtraction result is: " + subtraction(452,47));
+        System.out.println("Multiplication result is: " + multiplication(5,5));
+        System.out.println("Division result is: " + division(10,2));
+        System.out.println("Number average is: " + numberAverage(20,39, 75));
+        System.out.println("Temperature is: " + temperature(92));
 
     }
 
-    public static int adunare(int primulNumar, int alDoileaNumar){
+    public static int addition(int firstNumber, int secondNumber){
 
-        int rezultat = primulNumar + alDoileaNumar;
+        int result = firstNumber + secondNumber;
 
-        return rezultat;
+        return result;
 
-    } public static int scadere(int primulNumar, int alDoileaNumar){
+    } public static int subtraction(int firstNumber, int secondNumber){
 
-        int rezultat = primulNumar - alDoileaNumar;
+        int result = firstNumber - secondNumber;
 
-        return rezultat;
+        return result;
     }
 
-        public static int inmultire(int primulNumar, int alDoileaNumar){
+        public static int multiplication(int firstNumber, int secondNumber){
 
-        int rezultat = primulNumar * alDoileaNumar;
+        int result = firstNumber * secondNumber;
 
-        return rezultat;
+        return result;
     }
 
-        public static int impartire(int primulNumar, int alDoileaNumar){
+        public static int division(int firstNumber, int secondNumber){
 
-        int rezultat = primulNumar / alDoileaNumar;
+        int result = firstNumber / secondNumber;
 
-        return rezultat;
+        return result;
     }
 
-    public static int mediaNumerelor(int a, int b, int c) {
-        int rezultat=a+b+c/3;
-        return rezultat;
+    public static int numberAverage(int a, int b, int c) {
+        int result=a+b+c/3;
+        return result;
     }
 
-    public static double temperatura(int grade) {
-        double temp=  5/9 * (grade -32);
+    public static double temperature(int degrees) {
+        double temp=  5/9d * (degrees -32f);
                 return temp;
 
 
